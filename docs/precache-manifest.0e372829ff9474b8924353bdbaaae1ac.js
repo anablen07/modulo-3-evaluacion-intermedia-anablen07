307d7e53c73ca14cef5273fbc7673dd73a6951eb
Ana Belen Ferreira <ana@pcdeana.localdomain>
2020-04-07 10:36:48 +0200

    Fix project structure

---
+++ docs/precache-manifest.0e372829ff9474b8924353bdbaaae1ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3756b5375e8a006098968faeeed3a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97f7187df9fc9d7ec280",
+    "url": "/static/css/main.1b5b41b0.chunk.css"
+  },
+  {
+    "revision": "c8a0b8bc856b054ea564",
+    "url": "/static/js/2.790f66aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.790f66aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97f7187df9fc9d7ec280",
+    "url": "/static/js/main.ce2d548f.chunk.js"
+  },
+  {
+    "revision": "0be80e1afbff1830438e",
+    "url": "/static/js/runtime-main.21bddbfb.js"
+  }
+]);
